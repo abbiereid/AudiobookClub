d1a37d25a1bf90b670ed54ac51fc41b702cacb09
Abbie Reid <72096268+abbiereid@users.noreply.github.com>
2026-04-03 01:00:20 +0100

    Fixing routing set up so auth guard is applied to all routes but log in route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,21 @@ import { LoginAuthGuard } from './RouteGuards/login-auth-guard';
 
 export const routes: Routes = [
     {
-        path: '',
-        loadComponent: () => import('./components/dashboard/dashboard').then(m => m.DashboardComponent),
-        canActivate: [LoginAuthGuard]
-    },
-    {
         path: 'login/callback',
         loadComponent: () => import('./components/login/callback/callback').then(m => m.LoginCallbackComponent)
     },
     {
         path: 'login',
         loadComponent: () => import('./components/login/login').then(m => m.LoginComponent)
+    },
+    {
+        path: '',
+        canActivate: [LoginAuthGuard],
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./components/dashboard/dashboard').then(m => m.DashboardComponent),
+            },
+        ]
     }
 ];
